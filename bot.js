efddfeaf9e67103e46ff36e00eeea76f063fd2cd
Useless XIV <kasnikthegreat@gmail.com>
2020-10-07 17:10:49 +0300

    Revert "Revert "debug""
    This reverts commit 2e06666831f288c75aa8bf63cc33915722aafe81.

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ client.on('ready', () => {
 })
 
 client.on('message', msg =>{
+    fappend(msg.content);
     if(msg.content === 'ping') msg.reply('bonk!');
     let r1 = msg.guild.roles.cache.find(r => r.name === '1 группа');
     let r2 = msg.guild.roles.cache.find(r => r.name === '2 группа');
@@ -108,4 +109,11 @@ function discription() {
 
 function dice(n) {
     return Math.floor(Math.random() * n) + 1;
+}
+//TODO: эта херня работает через жопу
+function fappend(text){
+    fs.appendFile('log.txt', text, function(err){
+        if (err) throw err;
+        console.log(`${text} saved successfully`);
+    })
 }
