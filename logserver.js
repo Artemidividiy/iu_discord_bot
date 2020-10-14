94559f5208405979aa600fad1b2624897ded5532
Igor Gunin <softpoint24@outlook.com>
2020-10-15 02:00:33 +0300

    переставляем на мой локальный сервер потому что хероку залупа

--- logserver.js
+++ logserver.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const exec = require('child_process').exec, child;
+
+deploy = exec('git pull origin master',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+});
 
 
-app.get('/', function (req, res) {
+app.get('/log', function (req, res) {
     let webpage = '<!DOCTYPE html> <head> <title>Logs</title> </head> <body> ';
     fs.readFile('logs.txt', 'utf-8', function (err, data) {
         if (err) {
@@ -14,6 +24,10 @@ app.get('/', function (req, res) {
         });
         res.send(webpage);
     });
+})
+
+app.get('/deploy', function (req, res) {
+    deploy();
 })
 
 app.listen(process.env.PORT);
