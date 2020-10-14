e0d0a1cbdd2603c392df7095551dacfbaf558ddf
Useless XIV <kasnikthegreat@gmail.com>
2020-10-14 15:49:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/main/')
 def mainpg():
     f = open('log.txt', 'r')
     a = {str(i) : 0 for i in f}
     return a 
+
+if __name__ == '__main__' : app.run(threaded=True)
